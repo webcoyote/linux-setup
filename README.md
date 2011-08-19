eeed0032cf08789e76678a0264f48ef2843c4bb1
Patrick Wyatt <patrickjohnwyatt@gmail.com>
2011-08-19 00:36:56 -0700

    updated install, rvm, zsh

--- README.md
+++ README.md
@@ -9,19 +9,7 @@ Installation
 
 	# Configure computer
 	sudo apt-get install -qq -y curl
-	bash < <(curl -s https://raw.github.com/gist/12417bd6bd3fc1e07b31)
-
-	# Install zsh
-	sudo apt-get install -y zsh
-	git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
-	cp ~/bin/webcoyote-zshrc ~/.zshrc
-	chsh -s `which zsh`
-
-	# Install sublime text 2
-	sudo ~/bin/sublime-update.sh 2095
-
-	# Install rvm + ruby
-	~/bin/install-rvm-ruby.sh
+	bash < <(curl -s https://raw.github.com/webcoyote/linux-setup/master/bin/linux-setup.sh)
 
 Comments
 ========
--- bin/install-rvm-ruby.sh
+++ bin/install-rvm-ruby.sh
@@ -6,15 +6,19 @@ MY_GEM_VER=1.8.7
 
 # Install zlib (required for some ruby gems)
 sudo apt-get install -y --force-yes zlib1g-dev
+sudo apt-get install -y libxslt-dev libxml2-dev libsqlite3-dev openssl libssl-dev
+
 
 # Install rvm
 curl -s https://rvm.beginrescueend.com/install/rvm -o rvm-installer ; chmod +x rvm-installer ; rvm_bin_path=~/.rvm/bin rvm_man_path=~/.rvm/share/man ./rvm-installer ; rm rvm-installer
 source "$HOME/.rvm/scripts/rvm"
 rvm reload
 
+
 # Install ruby
 rvm pkg install zlib
-rvm install ${MY_RUBY_VER}
+rvm pkg install openssl
+rvm install ${MY_RUBY_VER} --with-openssl-dir=$HOME/.rvm/usr
 rvm use ${MY_RUBY_VER} --default
 ruby --version
 
@@ -38,3 +42,12 @@ gem install rake bundler chef --no-rdoc --no-ri
 
 # Set default gemset
 echo "rvm 1.9.2@global" > ~/dev/.rvmrc
+
+
+# Fix rails console
+sudo apt-get install -y libreadline5-dev libncurses5-dev
+cd ~/.rvm/src/${MY_RUBY_VER}/ext/readline
+ruby extconf.rb
+make
+make install
+
