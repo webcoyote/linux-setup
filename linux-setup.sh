afb95270d591aa9cd6243d19d2e7b66902257e5f
Patrick Wyatt <patrickjohnwyatt@gmail.com>
2011-08-19 23:36:38 -0700

    check for existence before git clone

--- linux-setup.sh
+++ linux-setup.sh
@@ -4,16 +4,14 @@
 
 
 # Make sure this script is not run with sudo
-if [ $(id -u) -eq 0 ]
-then
-echo 'ERROR: This script should not be run as sudo or root.'
+if [ $(id -u) -eq 0 ]; then
+  echo 'ERROR: This script should not be run as sudo or root.'
   exit
 fi
 
 
 # Generate SSH key if it doesn't exist
-if [ ! -r "$HOME/.ssh/id_rsa" ]
-then
+if [ ! -r "$HOME/.ssh/id_rsa" ]; then
   ssh-keygen -t rsa
 
   # Ubuntu workaround to fix these problems:
@@ -33,13 +31,13 @@ sudo apt-get install -y devilspie
 
 
 # get linux setup
-mkdir -p ~/dev
-cd ~/dev
-git clone git://github.com/webcoyote/linux-setup.git
+if [ ! -d "$HOME/dev/linux-setup" ]; then
+  mkdir -p ~/dev
+  git clone git://github.com/webcoyote/linux-setup.git ~/dev/linux-setup
+fi
 
 
-# copy home directory
-# includes zsh, autotest, etc.
+# copy home directory - includes gemrc, rvmrc, zshrc, autotest, bin/, devilspie/
 cp -R ~/dev/linux-setup/home/ ~
 
 
@@ -48,9 +46,10 @@ sudo ~/bin/sublime-update.sh 2095
 
 
 # Use zsh
-git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
-chsh -s `which zsh`
-
+if [ ! -d "$HOME/.oh-my-zsh" ]; then
+  git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+  chsh -s `which zsh`
+fi
 
 # Install rvm + ruby
 ~/bin/install-rvm-ruby.sh
